24c1d65125a4867a8c98314c67083506e13c43ad
Alex Rusin <alex@sunriseintegration.com>
2022-02-11 13:32:50 -0800

    add try catch to users.js

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,18 @@
 import User from '../../database/models/user';
+import logger from '../../services/logger';
 
 export default async function handler(req, res) {
-  const users = await User.findAll({
-    attributes: ['first_name', 'last_name', 'email'],
-    limit: 100,
-  });
-
-  res.status(200).json({ users });
+  try {
+    const users = await User.findAll({
+      attributes: ['first_name', 'last_name', 'email'],
+      limit: 100,
+    });
+    res.status(200).json({ users });
+  } catch (e) {
+    logger.error(e.stack);
+    res.status(400).json({
+      error_code: 'get_users',
+      message: e.message,
+    });
+  }
 }
